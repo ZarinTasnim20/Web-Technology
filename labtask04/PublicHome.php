38be8f8d446692a467dbf3d0890b42b465c8a77d
Zarin Tasnim <87434489+ZarinTasnim20@users.noreply.github.com>
2022-03-12 15:45:48 +0600

    labtask04
    labtask04

---
+++ labtask04/PublicHome.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>X Company</title>
+
+</head>
+<body>
+    <?php include 'header.php'; ?>
+<span style = "display:inline-block; width:100%;text-align:left; height: 30%; padding:10px">
+<?php
+echo "<h3>Welcome to X Company</h3></p>";
+
+?>
+</span> 
+<?php include 'footer.php';?>
+</body>
+</html>
