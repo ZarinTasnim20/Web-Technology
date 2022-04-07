28171056d70948e5af5091bfce01acaea0471a4b
Zarin Tasnim <87434489+ZarinTasnim20@users.noreply.github.com>
2022-04-07 23:58:10 +0600

    labtask_05
    mvc database

---
+++ lab_05/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Product</title>
+</head>
+<body>
+	<h1 style="color:blue">Product</h1>
+
+	<ul>
+	  <li><a href="views/add-product-view.php">Add Product</a></li>
+	  <li><a href="views/list-product-view.php">Display Product List</a></li>
+	  <li><a href="views/search-product-view.php">Search Product</a></li>
+	  <li><a href="views/delete-product-view.php">Delete Product</a></li>
+	</ul>  
+
+		
+</body>
+</html>
